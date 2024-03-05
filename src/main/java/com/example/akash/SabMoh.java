8448452ff2b1d4c618dcdf9d30e28394fbd09212
dopeakas <tiwariakshayldmnr@gmail.com>
2024-03-06 02:37:20 +0530

    first bro

---
+++ src/main/java/com/example/akash/SabMoh.java
@@ -0,0 +1,12 @@
+package com.example.akash;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SabMoh {
+
+	
+	public static void main(String[] args) {
+		System.out.println("londe");
+	}
+}
---
+++ src/main/java/com/example/akash/SpringProfilesExample41Application.java
@@ -0,0 +1,13 @@
+package com.example.akash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilesExample41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilesExample41Application.class, args);
+	}
+
+}
